aafe2431157a4fe02567316218e84cb88eab0738
rubanAlexUa <rubashka2021@gmail.com>
2025-02-26 16:13:36 +0200

    add changed code

--- src/main.js
+++ src/main.js
@@ -14,20 +14,27 @@ let searchQuery = '';
 form.addEventListener('submit', async e => {
   e.preventDefault();
   message.innerHTML = 'Wait, the image is loaded';
+
   searchQuery = input.value.trim();
+  if (!searchQuery) return;
+
   currentPage = 1;
   gallery.innerHTML = '';
+
+  btnLoadMore.classList.add('hidden');
+
   try {
     const images = await getImage(searchQuery, currentPage);
     if (images.hits.length === 0) {
       iziToast.error({
         position: 'topRight',
-        message: `Sorry, there are no images matching your search ${searchQuery}. Please try again!`,
+        message: `Sorry, there are no images matching your search "${searchQuery}". Please try again!`,
       });
       return;
     }
     make(images.hits);
-    if (images.totalHits > 40) {
+
+    if (currentPage < Math.ceil(images.totalHits / 40)) {
       btnLoadMore.classList.remove('hidden');
     }
   } catch (error) {
@@ -35,6 +42,7 @@ form.addEventListener('submit', async e => {
       position: 'topRight',
       message: error.message,
     });
+    btnLoadMore.classList.add('hidden');
   } finally {
     message.textContent = '';
   }
@@ -45,11 +53,14 @@ btnLoadMore.addEventListener('click', loadMore);
 async function loadMore() {
   currentPage++;
   message.innerHTML = 'Wait, the image is loaded';
+  btnLoadMore.disabled = true;
+
   try {
     const images = await getImage(searchQuery, currentPage);
     make(images.hits);
     smoothScroll();
-    if (currentPage === Math.ceil(images.totalHits / 40)) {
+
+    if (currentPage >= Math.ceil(images.totalHits / 40)) {
       btnLoadMore.classList.add('hidden');
       iziToast.info({
         position: 'topRight',
@@ -61,8 +72,10 @@ async function loadMore() {
       position: 'topRight',
       message: error.message,
     });
+    btnLoadMore.classList.add('hidden');
   } finally {
     message.textContent = '';
+    btnLoadMore.disabled = false;
   }
 }
 
